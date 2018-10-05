ed4fd71018a0a14e4aad22bdc0181d56ab1a6a95
MaxDreamer <Hi_maxin@icloud.com>
2018-10-05 09:50:46 +0800

    spring ioc example

---
+++ src/HelloMessage/IHelloMessage.java
@@ -0,0 +1,8 @@
+package HelloMessage;
+
+/**
+ * Created by 马昕 on 2018/10/5.
+ */
+public interface IHelloMessage {
+    public String sayHello();
+}
---
+++ src/HelloMessage/Person.java
@@ -0,0 +1,19 @@
+package HelloMessage;
+
+/**
+ * Created by 马昕 on 2018/10/5.
+ */
+public class Person {
+    private IHelloMessage helloMessage;
+
+    public IHelloMessage getHelloMessage() {
+        return helloMessage;
+    }
+
+    public void setHelloMessage(IHelloMessage helloMessage) {
+        this.helloMessage = helloMessage;
+    }
+    public String sayHello(){
+        return this.helloMessage.sayHello();
+    }
+}
